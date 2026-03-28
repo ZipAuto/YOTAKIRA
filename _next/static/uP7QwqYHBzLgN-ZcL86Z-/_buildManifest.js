80f7e3ca7bb8e3e38b09df3a6f395f18af966eee
queryops <gallilloss@gmail.com>
2026-03-28 14:05:29 -0500

    Updates

---
+++ _next/static/uP7QwqYHBzLgN-ZcL86Z-/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nombre-del-repositorio//_next/:path+",
+        "destination": "/nombre-del-repositorio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
